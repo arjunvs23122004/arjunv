90f3e5a9d812ae2ed2706d22e2cf5fb657c30e77
ASUS <ASUS@DESKTOP-9SFCPF0>
2025-09-25 20:37:49 +0530

    update new line

--- tnsif_java_arjunvs/src/basic/java/BasicJava.java
+++ tnsif_java_arjunvs/src/basic/java/BasicJava.java
@@ -5,6 +5,7 @@ public class BasicJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Github");
+		System.out.println("I donw my first day work successfully");
 
 	}
 
